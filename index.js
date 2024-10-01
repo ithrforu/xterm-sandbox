f7476555e592efa7550ed39b9ec6da776d3f4b89
Владимир Колесников <v.kolesnikov@ideco.ru>
2024-10-01 10:58:41 +0500

    Loading fonts and styles from index.html

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 import { Terminal } from '@xterm/xterm';
 import { FitAddon } from '@xterm/addon-fit';
-import '@xterm/xterm/css/xterm.css';
-import "@fontsource/roboto-mono"; 
-import "@fontsource/roboto-mono/400.css"; 
-import "@fontsource/roboto-mono/400-italic.css"; 
-
 
 const terminal = new Terminal({
   fontFamily: "Roboto Mono",
 });
 
-const fitAddon = new FitAddon();
 
+const fitAddon = new FitAddon();
 terminal.loadAddon(fitAddon);
 
 const terminalContainer = document.getElementById('xterm-container');
-
 terminal.open(terminalContainer);
 
 terminal.onKey(({ key }) => {
