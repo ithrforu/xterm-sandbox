202cc74213937188b66d1f73ff21102cd81731da
Владимир Колесников <v.kolesnikov@ideco.ru>
2024-10-01 16:49:49 +0500

    preload with cascadia mono

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 import { Terminal } from '@xterm/xterm';
 import { FitAddon } from '@xterm/addon-fit';
-import XtermWebfont from 'xterm-webfont'
 
-const terminal = new Terminal({
-  fontFamily: "Roboto Mono",
-});
+const initTerminal =()  => {
+  const terminal = new Terminal({
+    fontFamily: "Cascadia Mono",
+  });
 
+  const fitAddon = new FitAddon();
 
-const fitAddon = new FitAddon();
-terminal.loadAddon(fitAddon);
-terminal.loadAddon(new XtermWebfont());
+  terminal.loadAddon(fitAddon);
 
+  const terminalContainer = document.getElementById('xterm-container');
 
-const terminalContainer = document.getElementById('xterm-container');
-terminal.loadWebfontAndOpen(terminalContainer)
+  terminal.open(terminalContainer);
 
-terminal.onKey(({ key }) => {
-  terminal.write(key);
-});
+  terminal.onKey(({ key }) => {
+    terminal.write(key);
+  });
 
-const resizeObserver = new ResizeObserver(() => {
-  requestAnimationFrame(() => fitAddon.fit());
-});
+  const resizeObserver = new ResizeObserver(() => {
+    requestAnimationFrame(() => fitAddon.fit());
+  });
 
-resizeObserver.observe(terminalContainer);
+  resizeObserver.observe(terminalContainer);
+
+  // write test after some time...
+  setTimeout(() => {
+    terminal.write('0123456789\r\n');
+    terminal.write('Linux version 6.8.11-200.1725856906.16.65fc3bb9.utm.x86_64 (fedora@gentoo) (gcc (GCC) 13.3.1 20240522 (Red Hat 13.3.1-1), GNU ld version 2.40-14.');
+    terminal.write('\x1b[0;3m0123456789\r\n');
+    terminal.write('\x1b[1;3m0123456789\r\n');
+    terminal.write('\x1b[mЮжЮжЮжЮжЮж\r\n');
+    terminal.write('\x1b[1mЮжЮжЮжЮжЮж\r\n');
+    terminal.write('\x1b[0;3mЮжЮжЮжЮжЮж\r\n');
+    terminal.write('\x1b[1;3mЮжЮжЮжЮжЮж\r\n');
+  }, 100);
+}
+
+document.fonts.ready.then(
+  fontFaceSet => Promise.all(Array.from(fontFaceSet).map(el => el.load())).then(initTerminal)
+);
